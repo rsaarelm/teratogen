b8b36aa0a85a687a70787afbfddf3e97d6fff08b
Risto Saarelma <risto.saarelma@iki.fi>
2009-11-15 22:17:08 +0200

    Field2 code.

--- teratogen.go
+++ teratogen.go
@@ -1,11 +1,55 @@
 package main
 
-//import fmt "fmt"
+import fmt "fmt"
 import time "time"
 
 import tcod "tcod"
 
 const tickerWidth = 80;
+
+type Field2 interface {
+	Get(x, y int) (interface{}, bool);
+	Set(x, y int, item interface{});
+}
+
+type MapField2 struct {
+	data map[int] interface{};
+}
+
+func NewMapField2() (result *MapField2) {
+	result = new(MapField2);
+	result.data = make(map[int] interface{}, 32);
+	return;
+}
+
+const mapField2Dim = 8192;
+
+// XXX: Hacky mapping of points into a fixed-size rectangle, since we don't
+// have tuple keys.
+func (f2 *MapField2) encodePoint(x, y int) int {
+	return (x - mapField2Dim / 2) + (y - mapField2Dim / 2) * mapField2Dim;
+}
+
+func (f2 *MapField2) Contains(x, y int) bool {
+	return x >= -mapField2Dim / 2 && y >= -mapField2Dim / 2 &&
+		x < mapField2Dim / 2 && y < mapField2Dim / 2;
+}
+
+func (f2 *MapField2) Get(x, y int) (ret interface{}, found bool) {
+	if f2.Contains(x, y) {
+		ret, found = f2.data[f2.encodePoint(x, y)];
+	} else {
+		found = false;
+	}
+	return;
+}
+
+func (f2 *MapField2) Set(x, y int, item interface{}) {
+	if f2.Contains(x, y) {
+		f2.data[f2.encodePoint(x, y)] = item;
+	}
+}
+
 
 func eatPrefix(str string, length int) (result string) {
 	if len(str) < length {
@@ -29,6 +73,16 @@ func updateTicker(str string, lineLength int) string {
 }
 
 func main() {
+	area := NewMapField2();
+	area.Set(10, 10, "A");
+
+	fmt.Printf("Testing MapField2.\n");
+	test, found := area.Get(10, 10);
+	fmt.Printf("%v %v\n", test, found);
+
+	test, found = area.Get(11, 10);
+	fmt.Printf("%v %v\n", test, found);
+
 	tcod.Init(80, 50, "Teratogen");
 	tcod.SetForeColor(tcod.MakeColor(255, 255, 0));
 	tcod.PutChar(0, 0, 64, tcod.BkgndNone);
