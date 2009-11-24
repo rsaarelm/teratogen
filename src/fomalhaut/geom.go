88418598fbaa5dc793ffb2292913841d6a74f685
Risto Saarelma <risto.saarelma@iki.fi>
2009-11-24 18:57:20 +0200

    Vec2I was broken due to misunderstood value object semantics, oops. Fixed and removed less useful seeming methods.

--- src/fomalhaut/geom.go
+++ src/fomalhaut/geom.go
@@ -2,32 +2,38 @@ package fomalhaut
 
 import "math"
 
-type Vec2I [2]int32;
+type Vec2I [2]int;
 
-func (self Vec2I) Add(rhs Vec2I) {
-	self[0] += rhs[0];
-	self[1] += rhs[1];
+func (lhs Vec2I) Equals(rhs Vec2I) bool {
+	return lhs[0] == rhs[0] && lhs[1] == rhs[1];
 }
 
-func (self Vec2I) Subtract(rhs Vec2I) {
-	self[0] -= rhs[0];
-	self[1] -= rhs[1];
+func (lhs Vec2I) Plus(rhs Vec2I) (result Vec2I) {
+	return Vec2I{lhs[0] + rhs[0], lhs[1] + rhs[1]};
 }
 
-func (self Vec2I) ElemMultiply(rhs Vec2I) {
-	self[0] *= rhs[0];
-	self[1] *= rhs[1];
+func (lhs Vec2I) Minus(rhs Vec2I) (result Vec2I) {
+	return Vec2I{lhs[0] - rhs[0], lhs[1] - rhs[1]};
 }
 
-func (self Vec2I) ElemDivide(rhs Vec2I) {
-	self[0] /= rhs[0];
-	self[1] /= rhs[1];
-}
-
-func (self Vec2I) Dot(rhs Vec2I) int32 {
-	return self[0] * rhs[0] + self[1] * rhs[1];
+func (lhs Vec2I) Dot(rhs Vec2I) int {
+	return lhs[0] * rhs[0] + lhs[1] * rhs[1];
 }
 
 func (self Vec2I) Abs() float64 {
 	return math.Sqrt(float64(self.Dot(self)));
+}
+
+// Iterate points where 0 <= x < self[0] and 0 <= y < self[1].
+func (self Vec2I) Iter() <-chan Vec2I {
+	c := make(chan Vec2I);
+	go func() {
+		for y := 0; y < self[1]; y++ {
+			for x:= 0; x < self[0]; x++ {
+				c <- Vec2I{x, y};
+			}
+		}
+		close(c);
+	}();
+	return c;
 }
