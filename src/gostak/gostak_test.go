bb1dd20184d8029ddf93d23c586d44d48b7a6cca
Risto Saarelma <risto.saarelma@iki.fi>
2009-12-09 23:46:57 +0200

    Quotations seem to work.

--- src/gostak/gostak_test.go
+++ src/gostak/gostak_test.go
@@ -18,3 +18,32 @@ func TestArith(t *testing.T) {
 	if fg.Len() != 1 { t.Errorf("Bad stack len %v", fg.Len()); }
 	if fg.At(0).(float64) != float64(3.0) { t.Errorf("Bad top value %v", fg.At(0)); }
 }
+
+func TestQuotation(t *testing.T) {
+	fg := NewGostakState();
+	fg.DefineNativeWord(".", func (x interface{}) { fmt.Println(x); });
+	fg.DefineNativeWord("ifte", func (pred bool, t, e interface{}) {
+		if pred {
+			fg.Eval(t.([]GostakCell));
+		} else {
+			fg.Eval(e.([]GostakCell));
+		}
+	});
+
+	progn1 := []GostakCell{
+		GostakCell{LiteralBool, true},
+		GostakCell{Quotation, []GostakCell{GostakCell{LiteralNum, float64(10.0)}}},
+		GostakCell{Quotation, []GostakCell{GostakCell{LiteralNum, float64(20.0)}}},
+		GostakCell{Word, "ifte"}};
+	fg.Eval(progn1);
+	if fg.At(0).(float64) != float64(10.0) { t.Errorf("Bad top value %v", fg.At(0)); }
+
+	progn2 := []GostakCell{
+		GostakCell{LiteralBool, false},
+		GostakCell{Quotation, []GostakCell{GostakCell{LiteralNum, float64(10.0)}}},
+		GostakCell{Quotation, []GostakCell{GostakCell{LiteralNum, float64(20.0)}}},
+		GostakCell{Word, "ifte"}};
+	fg.Eval(progn2);
+	if fg.At(0).(float64) != float64(20.0) { t.Errorf("Bad top value %v", fg.At(0)); }
+
+}
