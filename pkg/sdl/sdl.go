3228a83d0c963518d85d457fa2f4af34707134f7
Risto Saarelma <risto.saarelma@iki.fi>
2009-12-12 12:42:51 +0200

    Generating SDL definition wrapper with godefs.

--- pkg/sdl/sdl.go
+++ pkg/sdl/sdl.go
@@ -2,17 +2,29 @@ package sdl
 
 /*
 #include <SDL.h>
-int sdl_doublebuf = SDL_DOUBLEBUF;
-int sdl_init_video = SDL_INIT_VIDEO;
+
 */
 import "C"
 
 func InitSdl(width, height int, title string, fullscreen bool) {
-	C.SDL_Init(C.Uint32(C.sdl_init_video));
+	C.SDL_Init(INIT_VIDEO);
 	C.SDL_SetVideoMode(C.int(width), C.int(height), C.int(32),
-		C.Uint32(C.sdl_doublebuf));
+		DOUBLEBUF);
 }
 
 func ExitSdl() {
 	C.SDL_Quit();
 }
+
+type SdlSurface struct {
+	surf *C.SDL_Surface;
+}
+
+func (self *SdlSurface) freeSurface() {
+	C.SDL_FreeSurface(self.surf);
+}
+
+//func (self *SdlSurface) make32BitSoftwareSurface(width, height int) {
+//	self.freeSurface();
+//	self.surf = C.SDL_CreateRGBSurface(
+//}
