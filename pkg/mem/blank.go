a8e4fd9ecfcc7a362b89d23cb5ca5914b227bb25
Risto Saarelma <risto.saarelma@iki.fi>
2009-12-20 12:20:49 +0200

    Fixed wrong behavior with pointer types in blank object factory.

--- pkg/mem/blank.go
+++ pkg/mem/blank.go
@@ -1,10 +1,16 @@
 package mem
 
 import (
-	"fmt"
 	"hyades/dbg"
 	"reflect"
 )
+
+func TypeName(obj interface{}) string {
+	val := reflect.Indirect(reflect.NewValue(obj))
+	typ := val.Type()
+	name := typ.Name()
+	return name
+}
 
 // A factory object that matches the typenames to the type values of
 // registered object types and is able to manufacture these objects given the
@@ -21,9 +27,8 @@ func NewBlankObjectFactory() (result *BlankObjectFactory) {
 
 // Register an object type in the factory.
 func (self *BlankObjectFactory) Register(example interface{}) {
-	val := reflect.Indirect(reflect.NewValue(example))
-	typ := val.Type()
-	name := typ.Name()
+	typ := reflect.Typeof(example)
+	name := TypeName(example)
 	dbg.Assert(name != "", "Unnamed type")
 	self.typenames[name] = typ
 }
@@ -32,24 +37,25 @@ func (self *BlankObjectFactory) Register(example interface{}) {
 func (self *BlankObjectFactory) Make(name string) interface{} {
 	typ, ok := self.typenames[name]
 	dbg.Assert(ok, "Unknown typename %v", name)
-	return reflect.MakeZero(typ).Interface()
+	return blankCopyOfType(typ).Interface()
 }
 
 // Make a null object of the same type as the parameter.
 func BlankCopy(obj interface{}) interface{} {
-	ptr, isPointer := reflect.NewValue(obj).(*reflect.PtrValue)
+	return blankCopyOfType(reflect.Typeof(obj)).Interface()
+}
+
+func blankCopyOfType(typ reflect.Type) reflect.Value {
+	ptr, isPointer := typ.(*reflect.PtrType)
 	if isPointer {
 		// If the value is a pointer, make a new pointer value that
 		// points to a new empty inner value.
-		val := reflect.MakeZero(ptr.Elem().Type())
-		wrap := reflect.MakeZero(ptr.Type()).(*reflect.PtrValue)
-		wrap.PointTo(val)
-		return wrap.Interface()
+		result := reflect.MakeZero(ptr).(*reflect.PtrValue)
+		result.PointTo(reflect.MakeZero(ptr.Elem()))
+		return result
 	} else {
 		// Otherwise just copy the value straight up.
-		typ := reflect.Typeof(obj)
-		fmt.Println(typ)
-		return reflect.MakeZero(typ).Interface()
+		return reflect.MakeZero(typ)
 	}
-	panic("BlankCopy")
+	panic("blankCopyFromValue")
 }
