057cf10ab4cb91f024d2979ce5660c4f4acfa053
Risto Saarelma <risto.saarelma@iki.fi>
2009-12-21 18:35:37 +0200

    Overhauling SDL module to make it look like draw.Context.

--- pkg/hyades/sdl/sdl.go
+++ pkg/hyades/sdl/sdl.go
@@ -7,298 +7,228 @@ package sdl
 import "C"
 
 import (
-	"fmt"
-	"io"
+	"exp/draw"
 	"hyades/dbg"
 	"hyades/event"
 	"image"
-	"image/png"
 	"os"
 	"unsafe"
 )
 
-func Init(width, height int, title string, fullscreen bool) {
+const bitsPerPixel = 32
+
+//////////////////////////////////////////////////////////////////
+// SDL Context object
+//////////////////////////////////////////////////////////////////
+
+type Sound interface {
+	Play()
+}
+
+type Context interface {
+	draw.Context
+
+	// Close Closes the SDL window and uninitializes SDL.
+	Close()
+
+	// Blit draws an image on the window. It is much more efficient if the
+	// image is implemented by a SDL surface.
+	Blit(img image.Image, x, y int)
+
+	// Efficintly fills a rectangle on the screen with uniform color.
+	FillRect(rect draw.Rectangle, col image.Color)
+
+	// Convert converts an image into a SDL surface
+	Convert(img image.Image) image.Image
+
+	// TODO: API for freeing surfaces created by Convert. This should be done by a GC finalizer.
+
+	// MakeSound converts wav file data into a SDL sound object.
+	MakeSound(wavData []byte) (result Sound, err os.Error)
+}
+
+type context struct {
+	screen *C.SDL_Surface
+}
+
+// NewWindow initializes SDL and returns a new SDL context.
+func NewWindow(width, height int, title string, fullscreen bool) (result Context, err os.Error) {
 	flags := int64(DOUBLEBUF)
 	if fullscreen {
 		flags |= FULLSCREEN
 	}
-	C.SDL_Init(INIT_VIDEO | INIT_AUDIO)
-	C.SDL_SetVideoMode(C.int(width), C.int(height), 32, C.Uint32(flags))
+	if C.SDL_Init(INIT_VIDEO|INIT_AUDIO) == C.int(-1) {
+		err = os.NewError(GetError())
+		return
+	}
+	screen := C.SDL_SetVideoMode(C.int(width), C.int(height), bitsPerPixel, C.Uint32(flags))
+	if screen == nil {
+		err = os.NewError(GetError())
+		return
+	}
 	C.SDL_EnableUNICODE(1)
 	initAudio()
+	result = &context{screen}
+	return
 }
 
-// 8-bit, 4000 Hz audio
-const audioRate = 4000
-const audioBytesPerSample = 1
-const audioChannels = 2
+func (self *context) Screen() draw.Image { return self.screen }
 
-// Return the preferred sample rate for audio samples.
-func AudioRateHz() uint32 { return audioRate }
+func (self *context) FlushImage() { C.SDL_Flip(self.screen) }
 
-// Return the preferred byte count, 1 or 2, of audio samples
-func AudioBytesPerSample() int { return audioBytesPerSample }
-
-func initAudio() {
-	var audioFormat C.Uint16
-	switch audioBytesPerSample {
-	case 1:
-		audioFormat = C.Uint16(AUDIO_U8)
-	case 2:
-		audioFormat = C.Uint16(AUDIO_U16SYS)
-	default:
-		dbg.Die("Bad audioBytesPerSample %v", audioBytesPerSample)
-	}
-
-	audioBuffers := C.int(4096)
-
-	ok := C.Mix_OpenAudio(C.int(audioRate), audioFormat, C.int(audioChannels), audioBuffers)
-
-	if ok != 0 {
-		panic("Mixer error" + GetError())
-	}
+func (self *context) KeyboardChan() <-chan int {
+	// TODO
+	return nil
 }
 
-func exitAudio() { C.Mix_CloseAudio() }
+func (self *context) MouseChan() <-chan draw.Mouse {
+	// TODO
+	return nil
+}
 
-func Exit() {
+func (self *context) ResizeChan() <-chan bool {
+	// TODO
+	return nil
+}
+
+func (self *context) QuitChan() <-chan bool {
+	// TODO
+	return nil
+}
+
+func (self *context) Close() {
 	exitAudio()
 	C.SDL_Quit()
 }
 
-type IntRect interface {
-	X() int
-	Y() int
-	Width() int
-	Height() int
-}
-
-func Rect(x, y int16, width, height uint16) *C.SDL_Rect {
-	return &C.SDL_Rect{C.Sint16(x), C.Sint16(y), C.Uint16(width), C.Uint16(height)}
-}
-
-func (self *C.SDL_Rect) X() int { return int(self.x) }
-
-func (self *C.SDL_Rect) Y() int { return int(self.y) }
-
-func (self *C.SDL_Rect) Width() int { return int(self.w) }
-
-func (self *C.SDL_Rect) Height() int { return int(self.h) }
-
-func (self *C.SDL_Rect) String() string {
-	return fmt.Sprintf("[%d %d - %d %d]", self.x, self.y, self.w, self.h)
-}
-
-func convertRect(rec IntRect) *C.SDL_Rect {
-	return &C.SDL_Rect{C.Sint16(rec.X()), C.Sint16(rec.Y()),
-		C.Uint16(rec.Width()), C.Uint16(rec.Height()),
+func (self *context) Blit(img image.Image, x, y int) {
+	if surface, isSurface := img.(*C.SDL_Surface); isSurface {
+		// It's a SDL surface, do a fast SDL blit.
+		rect := C.SDL_Rect{C.Sint16(x), C.Sint16(y), 0, 0}
+		C.SDL_BlitSurface(surface, nil, self.screen, &rect)
+	} else {
+		// It's something else, naively draw the individual pixels.
+		draw.Draw(self.Screen(),
+			draw.Rect(x, y, x+img.Width(), y+img.Height()), img, nil, draw.Pt(0, 0))
 	}
 }
 
+func (self *context) FillRect(rect draw.Rectangle, c image.Color) {
+	self.screen.FillRect(rect, c)
+}
+
+func (self *context) Convert(img image.Image) image.Image {
+	width, height := img.Width(), img.Height()
+
+	surf := C.SDL_CreateRGBSurface(self.screen.flags, C.int(width), C.int(height),
+		C.int(self.screen.format.BitsPerPixel), self.screen.format.Rmask,
+		self.screen.format.Gmask, self.screen.format.Bmask, self.screen.format.Amask)
+
+	draw.Draw(surf, draw.Rect(0, 0, width, height), img, nil, draw.Pt(0, 0))
+	return surf
+}
+
+func (self *context) MakeSound(wavData []byte) (result Sound, err os.Error) {
+	rw := C.SDL_RWFromMem(unsafe.Pointer(&wavData[0]), C.int(len(wavData)))
+	defer C.SDL_FreeRW(rw)
+	chunk := C.Mix_LoadWAV_RW(rw, 1)
+	if chunk == nil {
+		err = os.NewError(GetError())
+		return
+	}
+	result = chunk
+	return
+}
+
+//////////////////////////////////////////////////////////////////
+// Helper functions
+//////////////////////////////////////////////////////////////////
+
 func GetError() string { return C.GoString(C.SDL_GetError()) }
+
+func convertRect(rect draw.Rectangle) C.SDL_Rect {
+	rect = rect.Canon()
+	return C.SDL_Rect{C.Sint16(rect.Min.X),
+		C.Sint16(rect.Min.Y),
+		C.Uint16(rect.Max.X - rect.Min.X),
+		C.Uint16(rect.Max.Y - rect.Min.Y),
+	}
+}
 
 //////////////////////////////////////////////////////////////////
 // Video
 //////////////////////////////////////////////////////////////////
 
-func Flip() { C.SDL_Flip(C.SDL_GetVideoSurface()) }
-
-// XXX: This randomly crashes or freezes the application on my workstation.
-func ToggleFullScreen() {
-	vid := GetVideoSurface()
-	ok := C.SDL_WM_ToggleFullScreen(vid.surf)
-	if ok != 1 {
-		dbg.Warn("Couldn't toggle fullscreen: " + GetError())
-		return
+func (self *C.SDL_Surface) FreeSurface() {
+	if self != nil {
+		C.SDL_FreeSurface(self)
 	}
 }
 
-type Surface struct {
-	surf *C.SDL_Surface
-	// The area of the surface that'll be blitted. Assume entire surface
-	// if nil.
-	blitRect *C.SDL_Rect
-}
-
-func GetVideoSurface() (result *Surface) {
-	// XXX: This is pretty immutable, could be cached?
-	result = new(Surface)
-	result.surf = C.SDL_GetVideoSurface()
-	if result.surf == nil {
-		dbg.Die("Couldn't get video surface. " + GetError())
-	}
-	return
-}
-
-func Make32BitSurface(flags int, width, height int) (result *Surface) {
-	result = new(Surface)
-
-	var rmask, gmask, bmask, amask uint32
-
-	if BYTEORDER == BIG_ENDIAN {
-		rmask, gmask, bmask, amask = 0xff000000, 0x00ff0000, 0x0000ff00, 0x000000ff
-	} else {
-		rmask, gmask, bmask, amask = 0x000000ff, 0x0000ff00, 0x00ff0000, 0xff000000
-	}
-
-	result.surf = C.SDL_CreateRGBSurface(
-		C.Uint32(flags), C.int(width), C.int(height), 32,
-		C.Uint32(rmask), C.Uint32(gmask), C.Uint32(bmask), C.Uint32(amask))
-	// XXX: Need to init all to opaque alpha or blits won't set alpha.
-	result.FillRect(Rect(0, 0, uint16(result.Width()), uint16(result.Height())),
-		image.RGBAColor{0, 0, 0, 255})
-	return
-}
-
-func MakePngSurface(input io.Reader) (result *Surface, err os.Error) {
-	pic, err := png.Decode(input)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return MakeImageSurface(pic), nil
-}
-
-func MakeImageSurface(img image.Image) (result *Surface) {
-	result = Make32BitSurface(0, img.Width(), img.Height())
-	for x, w := 0, img.Width(); x < w; x++ {
-		for y, h := 0, img.Height(); y < h; y++ {
-			result.Set(x, y, img.At(x, y))
-		}
-	}
-	return result
-}
-
-func (self *Surface) FreeSurface() {
-	if self.surf != nil {
-		C.SDL_FreeSurface(self.surf)
-		self.surf = nil
-	}
-}
-
-func (self *Surface) Set(x, y int, c image.Color) {
+func (self *C.SDL_Surface) Set(x, y int, c image.Color) {
 	color := self.mapRGBA(c)
 
 	// XXX: Calling another method here is pretty slow probably. Also
 	// should unroll the loop for fixed ops for 1, 2, 3 and 4 bytes per
 	// pixel.
-	for i := 0; i < int(self.surf.format.BytesPerPixel); i++ {
+	for i := 0; i < int(self.format.BytesPerPixel); i++ {
 		self.writePixelData(self.pixelOffset(x, y)+i, byte(color%0x100))
 		color = color >> 8
 	}
 }
 
-func (self *Surface) FillRect(rec IntRect, c image.Color) {
-	C.SDL_FillRect(self.surf,
-		(*C.SDL_Rect)(unsafe.Pointer(convertRect(rec))),
+func (self *C.SDL_Surface) FillRect(rect draw.Rectangle, c image.Color) {
+	sdlRect := convertRect(rect)
+	C.SDL_FillRect(self,
+		&sdlRect,
 		C.Uint32(self.mapRGBA(c)))
 }
 
-func (self *Surface) mapRGBA(c image.Color) uint32 {
+func (self *C.SDL_Surface) mapRGBA(c image.Color) uint32 {
 	r32, g32, b32, a32 := c.RGBA()
 	// TODO: Compensate for pre-alphamultiplication from c.RGBA(), intensify RGB if A is low.
 	r, g, b, a := byte(r32>>24), byte(g32>>24), byte(b32>>24), byte(a32>>24)
 
-	return uint32(C.SDL_MapRGBA(self.surf.format,
+	return uint32(C.SDL_MapRGBA(self.format,
 		C.Uint8(r), C.Uint8(g), C.Uint8(b), C.Uint8(a)))
 }
 
-func (self *Surface) Blit(target *Surface, x, y int) {
+func (self *C.SDL_Surface) Blit(target *C.SDL_Surface, x, y int) {
 	rect := C.SDL_Rect{C.Sint16(x), C.Sint16(y), 0, 0}
-	C.SDL_BlitSurface(self.surf, self.blitRect, target.surf, &rect)
+	C.SDL_BlitSurface(self, nil, target, &rect)
 }
 
-func (self *Surface) BlitRect(target *Surface, area IntRect, x, y int) {
-	rect := C.SDL_Rect{C.Sint16(x), C.Sint16(y), 0, 0}
-	srcRect := convertRect(area)
-	C.SDL_BlitSurface(self.surf, srcRect, target.surf, &rect)
-}
+func (self *C.SDL_Surface) Width() int { return int(self.w) }
 
-func (self *Surface) Width() int {
-	if self.blitRect != nil {
-		return self.blitRect.Width()
-	}
-	return int(self.surf.w)
-}
+func (self *C.SDL_Surface) Height() int { return int(self.h) }
 
-func (self *Surface) Height() int {
-	if self.blitRect != nil {
-		return self.blitRect.Height()
-	}
-	return int(self.surf.h)
-}
-
-func (self *Surface) At(x, y int) image.Color {
-	bitMask := uint32(0xffffffff) >> (32 - self.surf.format.BitsPerPixel)
+func (self *C.SDL_Surface) At(x, y int) image.Color {
+	bitMask := uint32(0xffffffff) >> (32 - self.format.BitsPerPixel)
 	color := self.readPixelData(self.pixelOffset(x, y)) & uint32(bitMask)
 	var r, g, b, a byte
 	C.SDL_GetRGBA(C.Uint32(color),
-		self.surf.format,
+		self.format,
 		(*C.Uint8)(&r), (*C.Uint8)(&g), (*C.Uint8)(&b), (*C.Uint8)(&a))
 	return image.RGBAColor{r, g, b, a}
 }
 
 // For compliance wth the image.Image interface
-func (self *Surface) ColorModel() image.ColorModel {
+func (self *C.SDL_Surface) ColorModel() image.ColorModel {
 	return image.RGBAColorModel
 }
 
-// Convert the pixel format of this surface to match that of the other one.
-// Converting sprite surfaces to match the format of the display surface makes
-// blitting them much faster.
-func (self *Surface) Convert(other *Surface) {
-	// TODO: More graceful error handling.
-	newSurface := C.SDL_ConvertSurface(self.surf, other.surf.format,
-		C.Uint32(self.surf.flags))
-	if newSurface == nil {
-		panic("Surface conversion failed")
-	}
-	self.FreeSurface()
-	self.surf = newSurface
-}
-
-func (self *Surface) MakeTiles(width, height int, offsetX, offsetY int, gapX, gapY int) (result []*Surface) {
-	numX := (self.Width() - offsetX) / (width + gapX)
-	numY := (self.Height() - offsetY) / (width + gapY)
-
-	result = make([]*Surface, numX*numY)
-	i := 0
-
-	for y := 0; y < numY; y++ {
-		for x := 0; x < numX; x++ {
-			rect := Rect(int16(offsetX+x*(width+gapX)),
-				int16(offsetY+y*(height+gapY)),
-				uint16(width), uint16(height))
-			tile := &Surface{self.surf, rect}
-
-			result[i] = tile
-			i++
-		}
-	}
-	return
-}
-
-func (self *Surface) writePixelData(offset int, data byte) {
-	pixPtr := (uintptr)(unsafe.Pointer(self.surf.pixels)) + uintptr(offset)
+func (self *C.SDL_Surface) writePixelData(offset int, data byte) {
+	pixPtr := (uintptr)(unsafe.Pointer(self.pixels)) + uintptr(offset)
 	*(*byte)(unsafe.Pointer(pixPtr)) = data
 }
 
-func (self *Surface) readPixelData(offset int) uint32 {
-	pixPtr := (uintptr)(unsafe.Pointer(self.surf.pixels)) + uintptr(offset)
+func (self *C.SDL_Surface) readPixelData(offset int) uint32 {
+	pixPtr := (uintptr)(unsafe.Pointer(self.pixels)) + uintptr(offset)
 	return *(*uint32)(unsafe.Pointer(pixPtr))
 }
 
-func (self *Surface) pixelOffset(x, y int) int {
-	return y*int(self.surf.pitch) + x*int(self.surf.format.BytesPerPixel)
-}
-
-func (self *Surface) mustLock() bool {
-	// Reimplement this macro from SDL_video.h:
-	//#define SDL_MUSTLOCK(surface)   \
-	//  (surface->offset ||           \
-	//  ((surface->flags & (SDL_HWSURFACE|SDL_ASYNCBLIT|SDL_RLEACCEL)) != 0))
-	return self.surf.offset != 0 || self.surf.flags&(HWSURFACE|ASYNCBLIT|RLEACCEL) != 0
+func (self *C.SDL_Surface) pixelOffset(x, y int) int {
+	return y*int(self.pitch) + x*int(self.format.BytesPerPixel)
 }
 
 //////////////////////////////////////////////////////////////////
@@ -372,28 +302,43 @@ func eventType(evt *C.SDL_Event) byte {
 // Audio
 //////////////////////////////////////////////////////////////////
 
-type Sound struct {
-	chunk *C.Mix_Chunk
-}
+// 8-bit, 4000 Hz audio
+const audioRate = 4000
+const audioBytesPerSample = 1
+const audioChannels = 2
 
-func LoadWav(data []byte) (result *Sound, err os.Error) {
-	// XXX: This isn't working?
-	rw := C.SDL_RWFromMem(unsafe.Pointer(&data[0]), C.int(len(data)))
-	chunk := C.Mix_LoadWAV_RW(rw, 1)
-	if chunk == nil {
-		err = os.NewError(GetError())
-		return
+// Return the preferred sample rate for audio samples.
+func AudioRateHz() uint32 { return audioRate }
+
+// Return the preferred byte count, 1 or 2, of audio samples
+func AudioBytesPerSample() int { return audioBytesPerSample }
+
+func initAudio() {
+	var audioFormat C.Uint16
+	switch audioBytesPerSample {
+	case 1:
+		audioFormat = C.Uint16(AUDIO_U8)
+	case 2:
+		audioFormat = C.Uint16(AUDIO_U16SYS)
+	default:
+		dbg.Die("Bad audioBytesPerSample %v", audioBytesPerSample)
 	}
-	result = &Sound{chunk}
-	return
+
+	audioBuffers := C.int(4096)
+
+	ok := C.Mix_OpenAudio(C.int(audioRate), audioFormat, C.int(audioChannels), audioBuffers)
+
+	if ok != 0 {
+		panic("Mixer error" + GetError())
+	}
 }
 
-// Loops -1 plays forever, loops 0 plays once, loops 1 twice and so on.
-func (self *Sound) Play(loops int) { C.Mix_PlayChannelTimed(-1, self.chunk, C.int(loops), -1) }
+func exitAudio() { C.Mix_CloseAudio() }
 
-func (self *Sound) FreeSound() {
-	if self.chunk != nil {
-		C.Mix_FreeChunk(self.chunk)
-		self.chunk = nil
+func (self *C.Mix_Chunk) Play() { C.Mix_PlayChannelTimed(-1, self, C.int(0), -1) }
+
+func (self *C.Mix_Chunk) Free() {
+	if self != nil {
+		C.Mix_FreeChunk(self)
 	}
 }
