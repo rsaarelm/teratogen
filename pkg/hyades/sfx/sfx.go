5c12de1afe154e6d08f9cd4a16b117ab2ba9a819
Risto Saarelma <risto.saarelma@iki.fi>
2009-12-26 00:30:38 +0200

    More general wav maker.

--- pkg/hyades/sfx/sfx.go
+++ pkg/hyades/sfx/sfx.go
@@ -35,35 +35,85 @@ func MakeMono8Wav(wave WaveFunc, rateHz uint32, durationSec float64) []byte {
 		rateHz)
 }
 
-func MakeMono8WavFrom(data []byte, rateHz uint32) []byte {
+type NumChannels uint16
+
+const (
+	Mono   NumChannels = 1
+	Stereo NumChannels = 2
+)
+
+type SampleBytes uint16
+
+const (
+	Bit8  SampleBytes = 1
+	Bit16 SampleBytes = 2
+)
+
+type SampleRate uint32
+
+const (
+	Rate8k   SampleRate = 8000
+	Rate11k  SampleRate = 11025
+	Rate16k  SampleRate = 16000
+	Rate22k  SampleRate = 22050
+	Rate32k  SampleRate = 3200
+	Rate44k  SampleRate = 44100
+	Rate48k  SampleRate = 48000
+	Rate96k  SampleRate = 96000
+	Rate192k SampleRate = 192000
+)
+
+func writeWavRiff(out io.Writer, dataLen int) {
 	const headerLen = 44
 	const extraLength = headerLen - 8
 
-	numChannels := uint16(1)
-	bytesPerSecond := rateHz
-	bytesPerSample := uint16(1)
-	bitsPerSample := uint16(bytesPerSample * 8)
-
-	buf := new(bytes.Buffer)
-
-	// RIFF Chunk
-	buf.WriteString("RIFF")
+	io.WriteString(out, "RIFF")
 	// Write number of bytes to come. Length of data + headerLen - the 8
 	// bytes already written.
-	writeUint32(buf, uint32(len(data)+extraLength))
-	buf.WriteString("WAVE")
+	writeUint32(out, uint32(dataLen+extraLength))
+	io.WriteString(out, "WAVE")
+}
 
-	// FORMAT Chunk
-	buf.WriteString("fmt ")
+func writeWavFormat(out io.Writer, channels NumChannels, rate SampleRate, bytes SampleBytes) {
+	io.WriteString(out, "fmt ")
 	// FORMAT chunk length
-	writeUint32(buf, 16)
+	writeUint32(out, 16)
 	// unknown
-	writeUint16(buf, 1)
-	writeUint16(buf, numChannels)
-	writeUint32(buf, rateHz)
-	writeUint32(buf, bytesPerSecond)
-	writeUint16(buf, bytesPerSample)
-	writeUint16(buf, bitsPerSample)
+	writeUint16(out, 1)
+	writeUint16(out, uint16(channels))
+	writeUint32(out, uint32(rate))
+	writeUint32(out, uint32(rate)*uint32(channels)*uint32(bytes))
+	writeUint16(out, uint16(bytes))
+	writeUint16(out, 8*uint16(bytes))
+}
+
+func writeWavData(out io.Writer, dataLen int) {
+	io.WriteString(out, "data")
+	writeUint32(out, uint32(dataLen))
+}
+
+func WriteWav8(out io.Writer, channels NumChannels, rate SampleRate, data []byte) {
+	writeWavRiff(out, len(data))
+	writeWavFormat(out, channels, rate, Bit8)
+	writeWavData(out, len(data))
+	out.Write(data)
+}
+
+func WriteWav16(out io.Writer, channels NumChannels, rate SampleRate, data []uint16) {
+	writeWavRiff(out, len(data)*2)
+	writeWavFormat(out, channels, rate, Bit16)
+	writeWavData(out, len(data)*2)
+	for _, i := range data {
+		writeUint16(out, i)
+	}
+}
+
+func MakeMono8WavFrom(data []byte, rateHz uint32) []byte {
+	buf := new(bytes.Buffer)
+
+	writeWavRiff(buf, len(data))
+
+	writeWavFormat(buf, Mono, SampleRate(rateHz), Bit8)
 
 	// DATA Chunk
 	buf.WriteString("data")
