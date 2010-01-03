8c2e36d08116939dbf3f56a8c54086f18b9d259e
Risto Saarelma <risto.saarelma@iki.fi>
2010-01-03 21:55:45 +0200

    MouseExited method in MouseListener.

--- pkg/hyades/gui/gui.go
+++ pkg/hyades/gui/gui.go
@@ -42,6 +42,9 @@ type MouseListener interface {
 	// whether the event was consumed or if it should be passed on to the
 	// next widget.
 	HandleMouseEvent(area draw.Rectangle, event draw.Mouse) (consumed bool)
+
+	// MouseExited notifies the listener that the mouse has exited its area.
+	MouseExited(event draw.Mouse)
 }
 
 // DrawChildren is a helper function to iterate and draw the immediate
@@ -86,16 +89,22 @@ func WidgetsContaining(pos draw.Point, area draw.Rectangle, root Widget) iterabl
 
 // DispactchMouseEvent looks for widgets at where the mouse cursor is and that
 // can receive mouse events. Sends the mouse event to one, if found. Returns
-// whether a suitable widget was found.
-func DispatchMouseEvent(area draw.Rectangle, root Widget, event draw.Mouse) bool {
+// the receiver, if one was found, nil otherwise. The parameter
+// previousReceiver can be set to point to the MouseListener that received the
+// previous mouse event. If it's different than the currently found one, it
+// will be notified that the mouse has exited its area.
+func DispatchMouseEvent(area draw.Rectangle, root Widget, event draw.Mouse, previousReceiver MouseListener) MouseListener {
 	pos := draw.Pt(event.X, event.Y)
 	for pair := range WidgetsContaining(pos, area, root).Iter() {
 		area, widget := UnpackWidgetIteration(pair)
 		if mouseReceiver, ok := widget.(MouseListener); ok {
+			if previousReceiver != nil && mouseReceiver != previousReceiver {
+				previousReceiver.MouseExited(event)
+			}
 			if mouseReceiver.HandleMouseEvent(area, event) {
-				return true
+				return mouseReceiver
 			}
 		}
 	}
-	return false
+	return nil
 }
