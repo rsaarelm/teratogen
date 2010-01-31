45e5d5b23b68749ced6e0d4981820ccb1f4d6d02
Risto Saarelma <risto.saarelma@iki.fi>
2010-01-31 18:15:31 +0200

    A slightly syntactically better keyword args kludge.

--- cmd/teratogen/rules.go
+++ cmd/teratogen/rules.go
@@ -96,7 +96,10 @@ type entityPrototype struct {
 	Props    map[string]interface{}
 }
 
-func NewPrototype(name, parent, iconId string, class EntityClass, scarcity, minDepth int, a ...) (result *entityPrototype) {
+// Keyword argument emulation with maps
+type KW map[string]interface{}
+
+func NewPrototype(name, parent, iconId string, class EntityClass, scarcity, minDepth int, kwargs KW) (result *entityPrototype) {
 	result = new(entityPrototype)
 	result.Name = name
 	result.Parent = parent
@@ -105,12 +108,9 @@ func NewPrototype(name, parent, iconId string, class EntityClass, scarcity, minD
 	result.Scarcity = scarcity
 	result.MinDepth = minDepth
 
-	// Custom settings from varargs.
-	props := alg.UnpackEllipsis(a)
-	dbg.Assert(len(props)%2 == 0, "NewPrototype: Proplist length is odd.")
 	result.Props = make(map[string]interface{})
-	for i := 0; i < len(props); i += 2 {
-		result.Props[props[i].(string)] = props[i+1]
+	for k, v := range kwargs {
+		result.Props[k] = v
 	}
 	return
 }
@@ -152,54 +152,54 @@ func (self *entityPrototype) SpawnWeight(depth int) (result float64) {
 
 var prototypes = map[string]*entityPrototype{
 	// Base prototype for creatures.
-	"creature": NewPrototype("creature", "", "", EnemyEntityClass, -1, 0,
-		FlagObstacle, 1,
-		PropStrength, Fair,
-		PropToughness, Fair,
-		PropMeleeSkill, Fair,
-		PropScale, 0,
-		PropWounds, 0,
-		PropDensity, 0),
-	"protagonist": NewPrototype("protagonist", "creature", "chars:0", PlayerEntityClass, -1, 0,
-		PropStrength, Great,
-		PropToughness, Good,
-		PropMeleeSkill, Good),
-	"zombie": NewPrototype("zombie", "creature", "chars:1", EnemyEntityClass, 100, 0,
-		PropStrength, Fair,
-		PropToughness, Poor,
-		PropMeleeSkill, Fair),
-	"dogthing": NewPrototype("dog-thing", "creature", "chars:2", EnemyEntityClass, 150, 0,
-		PropStrength, Fair,
-		PropToughness, Fair,
-		PropMeleeSkill, Good,
-		PropScale, -1),
-	"ogre": NewPrototype("ogre", "creature", "chars:15", EnemyEntityClass, 600, 5,
-		PropStrength, Great,
-		PropToughness, Great,
-		PropMeleeSkill, Fair,
-		PropScale, 3),
-	"boss1": NewPrototype("elder spawn", "creature", "chars:5", EnemyEntityClass, 3000, 10,
-		PropStrength, Legendary,
-		PropToughness, Legendary,
-		PropMeleeSkill, Superb,
-		PropScale, 5),
-	"globe": NewPrototype("health globe", "", "items:1", GlobeEntityClass, 30, 0),
-	"plantpot": NewPrototype("plant pot", "", "items:3", ItemEntityClass, 200, 0),
-	"pistol": NewPrototype("pistol", "", "items:4", ItemEntityClass, 200, 0,
-		PropEquipmentSlot, PropGunWeaponGuid,
-		PropWoundBonus, 1,
-		PropDurability, 12),
-	"machete": NewPrototype("machete", "", "items:5", ItemEntityClass, 200, 0,
-		PropEquipmentSlot, PropMeleeWeaponGuid,
-		PropWoundBonus, 2,
-		PropDurability, 20),
-	"kevlar": NewPrototype("kevlar armor", "", "items:6", ItemEntityClass, 200, 0,
-		PropEquipmentSlot, PropBodyArmorGuid,
-		PropToughness, Good,
-		PropDefenseBonus, 1,
-		PropDurability, 20),
-	"medkit": NewPrototype("medkit", "", "items:7", ItemEntityClass, 200, 0,
-		PropItemUse, MedkitUse),
+	"creature": NewPrototype("creature", "", "", EnemyEntityClass, -1, 0, KW{
+		FlagObstacle: 1,
+		PropStrength: Fair,
+		PropToughness: Fair,
+		PropMeleeSkill: Fair,
+		PropScale: 0,
+		PropWounds: 0,
+		PropDensity: 0}),
+	"protagonist": NewPrototype("protagonist", "creature", "chars:0", PlayerEntityClass, -1, 0, KW{
+		PropStrength: Great,
+		PropToughness: Good,
+		PropMeleeSkill: Good}),
+	"zombie": NewPrototype("zombie", "creature", "chars:1", EnemyEntityClass, 100, 0, KW{
+		PropStrength: Fair,
+		PropToughness: Poor,
+		PropMeleeSkill: Fair}),
+	"dogthing": NewPrototype("dog-thing", "creature", "chars:2", EnemyEntityClass, 150, 0, KW{
+		PropStrength: Fair,
+		PropToughness: Fair,
+		PropMeleeSkill: Good,
+		PropScale: -1}),
+	"ogre": NewPrototype("ogre", "creature", "chars:15", EnemyEntityClass, 600, 5, KW{
+		PropStrength: Great,
+		PropToughness: Great,
+		PropMeleeSkill: Fair,
+		PropScale: 3}),
+	"boss1": NewPrototype("elder spawn", "creature", "chars:5", EnemyEntityClass, 3000, 10, KW{
+		PropStrength: Legendary,
+		PropToughness: Legendary,
+		PropMeleeSkill: Superb,
+		PropScale: 5}),
+	"globe": NewPrototype("health globe", "", "items:1", GlobeEntityClass, 30, 0, KW{}),
+	"plantpot": NewPrototype("plant pot", "", "items:3", ItemEntityClass, 200, 0, KW{}),
+	"pistol": NewPrototype("pistol", "", "items:4", ItemEntityClass, 200, 0, KW{
+		PropEquipmentSlot: PropGunWeaponGuid,
+		PropWoundBonus: 1,
+		PropDurability: 12}),
+	"machete": NewPrototype("machete", "", "items:5", ItemEntityClass, 200, 0, KW{
+		PropEquipmentSlot: PropMeleeWeaponGuid,
+		PropWoundBonus: 2,
+		PropDurability: 20}),
+	"kevlar": NewPrototype("kevlar armor", "", "items:6", ItemEntityClass, 200, 0, KW{
+		PropEquipmentSlot: PropBodyArmorGuid,
+		PropToughness: Good,
+		PropDefenseBonus: 1,
+		PropDurability: 20}),
+	"medkit": NewPrototype("medkit", "", "items:7", ItemEntityClass, 200, 0, KW{
+		PropItemUse: MedkitUse}),
 }
 
 func Log2Modifier(x int) int {
