85d184d7e551e34eea69722fff39d686bfd8bd6d
Risto Saarelma <risto.saarelma@iki.fi>
2012-07-28 09:41:28 +0300

    SDL bindings

---
+++ src/teratogen/sdl/keysym.go
@@ -0,0 +1,327 @@
+package sdl
+
+/*
+#include <SDL/SDL_keysym.h>
+*/
+import "C"
+
+import (
+	"fmt"
+)
+
+// Denotes a key on the keyboard, like 'a' or 'up arrow'.
+type KeySym int
+
+// Denotes a nonportable hardware scancode for a keyboard key. Independent of
+// keyboard layout.
+type Scancode int
+
+type KeyEvent struct {
+	Print rune // Printable character
+	Sym   KeySym
+	Code  Scancode
+	KeyUp bool // True if this event was a key being unpressed
+}
+
+func (sym KeySym) String() string {
+	if str, found := symNames[sym]; found {
+		return str
+	}
+	return fmt.Sprintf("KEY_%d", int(sym))
+}
+
+func TranslateScancode(code Scancode) KeySym {
+	if int(code) >= 0 && int(code) < len(scancodeMap) {
+		return scancodeMap[int(code)]
+	}
+	return K_UNKNOWN
+}
+
+// FixedKeySym tries to use the scancode of the key event to get a keyboard
+// layout invariant keysym. If this fails, it returns the unchanged keysym.
+func (evt KeyEvent) FixedSym() KeySym {
+	if t := TranslateScancode(evt.Code); t != K_UNKNOWN {
+		return t
+	}
+	return evt.Sym
+}
+
+// Expose SDL keysym constants
+const (
+	K_UNKNOWN      KeySym = C.SDLK_UNKNOWN
+	K_FIRST               = C.SDLK_FIRST
+	K_BACKSPACE           = C.SDLK_BACKSPACE
+	K_TAB                 = C.SDLK_TAB
+	K_CLEAR               = C.SDLK_CLEAR
+	K_RETURN              = C.SDLK_RETURN
+	K_PAUSE               = C.SDLK_PAUSE
+	K_ESCAPE              = C.SDLK_ESCAPE
+	K_SPACE               = C.SDLK_SPACE
+	K_EXCLAIM             = C.SDLK_EXCLAIM
+	K_QUOTEDBL            = C.SDLK_QUOTEDBL
+	K_HASH                = C.SDLK_HASH
+	K_DOLLAR              = C.SDLK_DOLLAR
+	K_AMPERSAND           = C.SDLK_AMPERSAND
+	K_QUOTE               = C.SDLK_QUOTE
+	K_LEFTPAREN           = C.SDLK_LEFTPAREN
+	K_RIGHTPAREN          = C.SDLK_RIGHTPAREN
+	K_ASTERISK            = C.SDLK_ASTERISK
+	K_PLUS                = C.SDLK_PLUS
+	K_COMMA               = C.SDLK_COMMA
+	K_MINUS               = C.SDLK_MINUS
+	K_PERIOD              = C.SDLK_PERIOD
+	K_SLASH               = C.SDLK_SLASH
+	K_0                   = C.SDLK_0
+	K_1                   = C.SDLK_1
+	K_2                   = C.SDLK_2
+	K_3                   = C.SDLK_3
+	K_4                   = C.SDLK_4
+	K_5                   = C.SDLK_5
+	K_6                   = C.SDLK_6
+	K_7                   = C.SDLK_7
+	K_8                   = C.SDLK_8
+	K_9                   = C.SDLK_9
+	K_COLON               = C.SDLK_COLON
+	K_SEMICOLON           = C.SDLK_SEMICOLON
+	K_LESS                = C.SDLK_LESS
+	K_EQUALS              = C.SDLK_EQUALS
+	K_GREATER             = C.SDLK_GREATER
+	K_QUESTION            = C.SDLK_QUESTION
+	K_AT                  = C.SDLK_AT
+	K_LEFTBRACKET         = C.SDLK_LEFTBRACKET
+	K_BACKSLASH           = C.SDLK_BACKSLASH
+	K_RIGHTBRACKET        = C.SDLK_RIGHTBRACKET
+	K_CARET               = C.SDLK_CARET
+	K_UNDERSCORE          = C.SDLK_UNDERSCORE
+	K_BACKQUOTE           = C.SDLK_BACKQUOTE
+	K_a                   = C.SDLK_a
+	K_b                   = C.SDLK_b
+	K_c                   = C.SDLK_c
+	K_d                   = C.SDLK_d
+	K_e                   = C.SDLK_e
+	K_f                   = C.SDLK_f
+	K_g                   = C.SDLK_g
+	K_h                   = C.SDLK_h
+	K_i                   = C.SDLK_i
+	K_j                   = C.SDLK_j
+	K_k                   = C.SDLK_k
+	K_l                   = C.SDLK_l
+	K_m                   = C.SDLK_m
+	K_n                   = C.SDLK_n
+	K_o                   = C.SDLK_o
+	K_p                   = C.SDLK_p
+	K_q                   = C.SDLK_q
+	K_r                   = C.SDLK_r
+	K_s                   = C.SDLK_s
+	K_t                   = C.SDLK_t
+	K_u                   = C.SDLK_u
+	K_v                   = C.SDLK_v
+	K_w                   = C.SDLK_w
+	K_x                   = C.SDLK_x
+	K_y                   = C.SDLK_y
+	K_z                   = C.SDLK_z
+	K_DELETE              = C.SDLK_DELETE
+	K_KP0                 = C.SDLK_KP0
+	K_KP1                 = C.SDLK_KP1
+	K_KP2                 = C.SDLK_KP2
+	K_KP3                 = C.SDLK_KP3
+	K_KP4                 = C.SDLK_KP4
+	K_KP5                 = C.SDLK_KP5
+	K_KP6                 = C.SDLK_KP6
+	K_KP7                 = C.SDLK_KP7
+	K_KP8                 = C.SDLK_KP8
+	K_KP9                 = C.SDLK_KP9
+	K_KP_PERIOD           = C.SDLK_KP_PERIOD
+	K_KP_DIVIDE           = C.SDLK_KP_DIVIDE
+	K_KP_MULTIPLY         = C.SDLK_KP_MULTIPLY
+	K_KP_MINUS            = C.SDLK_KP_MINUS
+	K_KP_PLUS             = C.SDLK_KP_PLUS
+	K_KP_ENTER            = C.SDLK_KP_ENTER
+	K_KP_EQUALS           = C.SDLK_KP_EQUALS
+	K_UP                  = C.SDLK_UP
+	K_DOWN                = C.SDLK_DOWN
+	K_RIGHT               = C.SDLK_RIGHT
+	K_LEFT                = C.SDLK_LEFT
+	K_INSERT              = C.SDLK_INSERT
+	K_HOME                = C.SDLK_HOME
+	K_END                 = C.SDLK_END
+	K_PAGEUP              = C.SDLK_PAGEUP
+	K_PAGEDOWN            = C.SDLK_PAGEDOWN
+	K_F1                  = C.SDLK_F1
+	K_F2                  = C.SDLK_F2
+	K_F3                  = C.SDLK_F3
+	K_F4                  = C.SDLK_F4
+	K_F5                  = C.SDLK_F5
+	K_F6                  = C.SDLK_F6
+	K_F7                  = C.SDLK_F7
+	K_F8                  = C.SDLK_F8
+	K_F9                  = C.SDLK_F9
+	K_F10                 = C.SDLK_F10
+	K_F11                 = C.SDLK_F11
+	K_F12                 = C.SDLK_F12
+	K_F13                 = C.SDLK_F13
+	K_F14                 = C.SDLK_F14
+	K_F15                 = C.SDLK_F15
+	K_NUMLOCK             = C.SDLK_NUMLOCK
+	K_CAPSLOCK            = C.SDLK_CAPSLOCK
+	K_SCROLLOCK           = C.SDLK_SCROLLOCK
+	K_RSHIFT              = C.SDLK_RSHIFT
+	K_LSHIFT              = C.SDLK_LSHIFT
+	K_RCTRL               = C.SDLK_RCTRL
+	K_LCTRL               = C.SDLK_LCTRL
+	K_RALT                = C.SDLK_RALT
+	K_LALT                = C.SDLK_LALT
+	K_RMETA               = C.SDLK_RMETA
+	K_LMETA               = C.SDLK_LMETA
+	K_LSUPER              = C.SDLK_LSUPER
+	K_RSUPER              = C.SDLK_RSUPER
+	K_MODE                = C.SDLK_MODE
+	K_COMPOSE             = C.SDLK_COMPOSE
+	K_HELP                = C.SDLK_HELP
+	K_PRINT               = C.SDLK_PRINT
+	K_SYSREQ              = C.SDLK_SYSREQ
+	K_BREAK               = C.SDLK_BREAK
+	K_MENU                = C.SDLK_MENU
+	K_POWER               = C.SDLK_POWER
+	K_EURO                = C.SDLK_EURO
+	K_UNDO                = C.SDLK_UNDO
+	K_LAST                = C.SDLK_LAST
+)
+
+var symNames = map[KeySym]string{
+	K_BACKSPACE:    "BACKSPACE",
+	K_TAB:          "TAB",
+	K_CLEAR:        "CLEAR",
+	K_RETURN:       "RETURN",
+	K_PAUSE:        "PAUSE",
+	K_ESCAPE:       "ESCAPE",
+	K_SPACE:        "SPACE",
+	K_EXCLAIM:      "EXCLAIM",
+	K_QUOTEDBL:     "QUOTEDBL",
+	K_HASH:         "HASH",
+	K_DOLLAR:       "DOLLAR",
+	K_AMPERSAND:    "AMPERSAND",
+	K_QUOTE:        "QUOTE",
+	K_LEFTPAREN:    "LEFTPAREN",
+	K_RIGHTPAREN:   "RIGHTPAREN",
+	K_ASTERISK:     "ASTERISK",
+	K_PLUS:         "PLUS",
+	K_COMMA:        "COMMA",
+	K_MINUS:        "MINUS",
+	K_PERIOD:       "PERIOD",
+	K_SLASH:        "SLASH",
+	K_0:            "0",
+	K_1:            "1",
+	K_2:            "2",
+	K_3:            "3",
+	K_4:            "4",
+	K_5:            "5",
+	K_6:            "6",
+	K_7:            "7",
+	K_8:            "8",
+	K_9:            "9",
+	K_COLON:        "COLON",
+	K_SEMICOLON:    "SEMICOLON",
+	K_LESS:         "LESS",
+	K_EQUALS:       "EQUALS",
+	K_GREATER:      "GREATER",
+	K_QUESTION:     "QUESTION",
+	K_AT:           "AT",
+	K_LEFTBRACKET:  "LEFTBRACKET",
+	K_BACKSLASH:    "BACKSLASH",
+	K_RIGHTBRACKET: "RIGHTBRACKET",
+	K_CARET:        "CARET",
+	K_UNDERSCORE:   "UNDERSCORE",
+	K_BACKQUOTE:    "BACKQUOTE",
+	K_a:            "A",
+	K_b:            "B",
+	K_c:            "C",
+	K_d:            "D",
+	K_e:            "E",
+	K_f:            "F",
+	K_g:            "G",
+	K_h:            "H",
+	K_i:            "I",
+	K_j:            "J",
+	K_k:            "K",
+	K_l:            "L",
+	K_m:            "M",
+	K_n:            "N",
+	K_o:            "O",
+	K_p:            "P",
+	K_q:            "Q",
+	K_r:            "R",
+	K_s:            "S",
+	K_t:            "T",
+	K_u:            "U",
+	K_v:            "V",
+	K_w:            "W",
+	K_x:            "X",
+	K_y:            "Y",
+	K_z:            "Z",
+	K_DELETE:       "DELETE",
+	K_KP0:          "KP0",
+	K_KP1:          "KP1",
+	K_KP2:          "KP2",
+	K_KP3:          "KP3",
+	K_KP4:          "KP4",
+	K_KP5:          "KP5",
+	K_KP6:          "KP6",
+	K_KP7:          "KP7",
+	K_KP8:          "KP8",
+	K_KP9:          "KP9",
+	K_KP_PERIOD:    "KP_PERIOD",
+	K_KP_DIVIDE:    "KP_DIVIDE",
+	K_KP_MULTIPLY:  "KP_MULTIPLY",
+	K_KP_MINUS:     "KP_MINUS",
+	K_KP_PLUS:      "KP_PLUS",
+	K_KP_ENTER:     "KP_ENTER",
+	K_KP_EQUALS:    "KP_EQUALS",
+	K_UP:           "UP",
+	K_DOWN:         "DOWN",
+	K_RIGHT:        "RIGHT",
+	K_LEFT:         "LEFT",
+	K_INSERT:       "INSERT",
+	K_HOME:         "HOME",
+	K_END:          "END",
+	K_PAGEUP:       "PAGEUP",
+	K_PAGEDOWN:     "PAGEDOWN",
+	K_F1:           "F1",
+	K_F2:           "F2",
+	K_F3:           "F3",
+	K_F4:           "F4",
+	K_F5:           "F5",
+	K_F6:           "F6",
+	K_F7:           "F7",
+	K_F8:           "F8",
+	K_F9:           "F9",
+	K_F10:          "F10",
+	K_F11:          "F11",
+	K_F12:          "F12",
+	K_F13:          "F13",
+	K_F14:          "F14",
+	K_F15:          "F15",
+	K_NUMLOCK:      "NUMLOCK",
+	K_CAPSLOCK:     "CAPSLOCK",
+	K_SCROLLOCK:    "SCROLLOCK",
+	K_RSHIFT:       "RSHIFT",
+	K_LSHIFT:       "LSHIFT",
+	K_RCTRL:        "RCTRL",
+	K_LCTRL:        "LCTRL",
+	K_RALT:         "RALT",
+	K_LALT:         "LALT",
+	K_RMETA:        "RMETA",
+	K_LMETA:        "LMETA",
+	K_LSUPER:       "LSUPER",
+	K_RSUPER:       "RSUPER",
+	K_MODE:         "MODE",
+	K_COMPOSE:      "COMPOSE",
+	K_HELP:         "HELP",
+	K_PRINT:        "PRINT",
+	K_SYSREQ:       "SYSREQ",
+	K_BREAK:        "BREAK",
+	K_MENU:         "MENU",
+	K_POWER:        "POWER",
+	K_EURO:         "EURO",
+	K_UNDO:         "UNDO",
+}
---
+++ src/teratogen/sdl/sdl.go
@@ -0,0 +1,253 @@
+package sdl
+
+/*
+#cgo pkg-config: sdl
+#include <SDL/SDL.h>
+*/
+import "C"
+
+import (
+	"image"
+	"image/color"
+	"reflect"
+	"unsafe"
+)
+
+type MouseEvent struct {
+	Pos     image.Point
+	Buttons int8
+}
+
+type Surface struct {
+	ptr *C.SDL_Surface
+}
+
+// Pixels returns a byte slice memory-mapped to the pixel buffer of the
+// surface.
+func (s *Surface) Pixels() (result []byte) {
+	size := int(s.ptr.pitch) * int(s.ptr.h)
+
+	result = []byte{}
+	header := (*reflect.SliceHeader)(unsafe.Pointer(&result))
+	*header = reflect.SliceHeader{uintptr(s.ptr.pixels), size, size}
+	return
+}
+
+// Pixels32 is the same as Pixels, except that it returns an array of 32-bit
+// values. This is convenient for pixel manipulation of 32-bit color surfaces.
+func (s *Surface) Pixels32() (result []uint32) {
+	size := int(s.ptr.pitch) * int(s.ptr.h) / 4
+
+	result = []uint32{}
+	header := (*reflect.SliceHeader)(unsafe.Pointer(&result))
+	*header = reflect.SliceHeader{uintptr(s.ptr.pixels), size, size}
+	return
+}
+
+func (s *Surface) Size() image.Point {
+	return image.Pt(int(s.ptr.w), int(s.ptr.h))
+}
+
+// Pitch returns the byte span of a horizontal line in the surface.
+func (s *Surface) Pitch() int {
+	return int(s.ptr.pitch)
+}
+
+// Pitch32 returns the span of a horizontal line in the surface in 32-bit units.
+func (s *Surface) Pitch32() int {
+	pitch := int(s.ptr.pitch)
+	if pitch%4 != 0 {
+		panic("Pitch not divisible by 4")
+	}
+	return pitch / 4
+}
+
+// BPP returns the bytes per pixel of the surface.
+func (s *Surface) BPP() int {
+	return int(s.ptr.format.BytesPerPixel)
+}
+
+// MapColor converts color data into the internal format of the surface.
+func (s *Surface) MapColor(c color.Color) uint32 {
+	r, g, b, a := c.RGBA()
+	r8, g8, b8, a8 := C.Uint8(r>>8), C.Uint8(g>>8), C.Uint8(b>>8), C.Uint8(a>>8)
+	return uint32(C.SDL_MapRGBA(s.ptr.format, r8, g8, b8, a8))
+}
+
+var kbd chan KeyEvent
+var mouse chan MouseEvent
+var quit chan bool
+
+var exitChan chan bool
+
+const keyBufferSize = 16
+
+// Open starts a windowed SDL application.
+func Open(width, height int) {
+	initFlags := int64(C.SDL_INIT_VIDEO)
+	screenFlags := 0
+
+	if C.SDL_Init(C.Uint32(initFlags)) == C.int(-1) {
+		panic(getError())
+	}
+
+	screen := C.SDL_SetVideoMode(
+		C.int(width), C.int(height), 32, C.Uint32(screenFlags))
+	if screen == nil {
+		panic(getError())
+	}
+	C.SDL_EnableUNICODE(1)
+
+	kbd = make(chan KeyEvent, keyBufferSize)
+	mouse = make(chan MouseEvent, 1)
+	quit = make(chan bool, 1)
+	exitChan = make(chan bool)
+
+	go eventLoop()
+
+	return
+}
+
+// KeyboardChan returns a channel which emits keyboard events from the SDL
+// event loop.
+func KeyboardChan() <-chan KeyEvent {
+	return kbd
+}
+
+// MouseChan returns a channel which emits mouse events from the SDL
+// event loop.
+func MouseChan() <-chan MouseEvent {
+	return mouse
+}
+
+// QuitChan returns a channel which emits close application events from the
+// SDL event loop.
+func QuitChan() <-chan bool { return quit }
+
+// Close exits the SDL application.
+func Close() {
+	exitChan <- true
+	// Wait for the event loop to finish and close SDL. The program may exit
+	// without calling SDL_Quit if this isn't done.
+	_ = <-exitChan
+}
+
+// Video returns the surface for the base SDL window.
+func Video() *Surface {
+	return &Surface{C.SDL_GetVideoSurface()}
+}
+
+// Flip swaps screen buffers with a double-buffered display mode. Use it to
+// make the changes you made to the screen become visible.
+func Flip() {
+	C.SDL_Flip(C.SDL_GetVideoSurface())
+}
+
+func FillRect(rect image.Rectangle, color color.Color) {
+	sdlRect := convertRect(rect)
+	C.SDL_FillRect(C.SDL_GetVideoSurface(), &sdlRect, convertColor(color))
+}
+
+func Clear(color color.Color) {
+	C.SDL_FillRect(C.SDL_GetVideoSurface(), nil, convertColor(color))
+}
+
+func IsKeyDown(key int) bool {
+	var numKeys C.int
+	keys := C.SDL_GetKeyState(&numKeys)
+
+	if key < 0 || key >= int(numKeys) {
+		return false
+	}
+
+	// Keys is a byte array
+	// XXX: Is this idiomatic for accessing C arrays?
+	result := (*byte)(unsafe.Pointer(uintptr(unsafe.Pointer(keys)) + uintptr(key)))
+
+	return *result != 0
+}
+
+const DefaultRepeatDelay = C.SDL_DEFAULT_REPEAT_DELAY
+const DefaultRepeatInterval = C.SDL_DEFAULT_REPEAT_INTERVAL
+
+func EnableKeyRepeat(delay int, interval int) (ok bool) {
+	return int(C.SDL_EnableKeyRepeat(C.int(delay), C.int(interval))) == 0
+}
+
+func eventLoop() {
+	var evt C.SDL_Event
+
+	defer func() { exitChan <- true }()
+	defer C.SDL_Quit()
+
+	for {
+		select {
+		case _ = <-exitChan:
+			return
+		default:
+		}
+
+		if C.SDL_WaitEvent(&evt) != 0 {
+			switch typ := eventType(&evt); typ {
+			case C.SDL_KEYDOWN, C.SDL_KEYUP:
+				keyEvt := ((*C.SDL_KeyboardEvent)(unsafe.Pointer(&evt)))
+
+				data := KeyEvent{
+					rune(keyEvt.keysym.unicode),
+					KeySym(keyEvt.keysym.sym),
+					Scancode(keyEvt.keysym.scancode),
+					typ == C.SDL_KEYUP}
+
+				select {
+				case kbd <- data: // Send new key
+				case _ = <-kbd: // Buffer full, drop oldest key, then send
+					kbd <- data
+				default:
+				}
+
+			case C.SDL_MOUSEMOTION, C.SDL_MOUSEBUTTONUP, C.SDL_MOUSEBUTTONDOWN:
+				motEvt := ((*C.SDL_MouseMotionEvent)(unsafe.Pointer(&evt)))
+				data := MouseEvent{image.Pt(int(motEvt.x), int(motEvt.y)), int8(motEvt.state)}
+
+				select {
+				case mouse <- data:
+				default:
+				}
+			case C.SDL_QUIT:
+				quit <- true
+			}
+		}
+	}
+}
+
+func getError() string {
+	return C.GoString(C.SDL_GetError())
+}
+
+// Cgo can't access the "type" field of the SDL_Event union type directly
+// because it collides with a Go reserved word. This function casts the union
+// into a minimal struct that contains only the leading type byte and returns
+// the byte.
+func eventType(evt *C.SDL_Event) byte {
+	return byte(((*struct {
+		_type C.Uint8
+	})(unsafe.Pointer(evt)))._type)
+}
+
+func convertRect(rect image.Rectangle) C.SDL_Rect {
+	rect = rect.Canon()
+	return C.SDL_Rect{
+		C.Sint16(rect.Min.X),
+		C.Sint16(rect.Min.Y),
+		C.Uint16(rect.Max.X - rect.Min.X),
+		C.Uint16(rect.Max.Y - rect.Min.Y),
+	}
+}
+
+func convertColor(c color.Color) C.Uint32 {
+	r, g, b, a := c.RGBA()
+	r8, g8, b8, a8 := C.Uint8(r>>8), C.Uint8(g>>8), C.Uint8(b>>8), C.Uint8(a>>8)
+	return C.SDL_MapRGBA(
+		C.SDL_GetVideoSurface().format,
+		r8, g8, b8, a8)
+}
