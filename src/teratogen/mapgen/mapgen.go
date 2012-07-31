0a8a016707fd88fd231bf8d2cf9a751e44179f73
Risto Saarelma <risto.saarelma@iki.fi>
2012-07-31 19:47:50 +0300

    BSP mapgen adds doorways

--- src/teratogen/mapgen/mapgen.go
+++ src/teratogen/mapgen/mapgen.go
@@ -40,12 +40,30 @@ type Former interface {
 // BspRooms builds an indoor rooms style map using a binary split tree
 // algorithm.
 func BspRooms(t Former, bounds image.Rectangle) {
+	const extraDoorChance = 1.0 / 128
+
+	bsp(t, bounds)
+
+	// Put some extra doorways on the map to make the structure more
+	// interesting; the standard minimal connectivity-ensuring doors produce
+	// no cycles, which makes the map less interesting for gameplay.
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			pt := image.Pt(x, y)
+			if isDoorSite(t, pt) && rand.Float64() < extraDoorChance {
+				t.Set(pt, Doorway)
+			}
+		}
+	}
+}
+
+func bsp(t Former, bounds image.Rectangle) {
 	if bounds.Dx() < 1 || bounds.Dy() < 1 {
 		return
 	}
 
 	const minArea = 8
-	const maxArea = 128
+	const maxArea = 96
 
 	area := bounds.Dx() * bounds.Dy()
 
@@ -57,12 +75,31 @@ func BspRooms(t Former, bounds image.Rectangle) {
 	digRoom(t, bounds)
 }
 
+func isDoorSite(t Former, pt image.Point) bool {
+	if t.At(pt) != Solid {
+		return false
+	}
+
+	up := t.At(pt.Add(image.Pt(0, -1)))
+	down := t.At(pt.Add(image.Pt(0, 1)))
+	left := t.At(pt.Add(image.Pt(-1, 0)))
+	right := t.At(pt.Add(image.Pt(1, 0)))
+
+	if up != down || left != right {
+		return false
+	}
+
+	return (up == Open && left == Solid) || (up == Solid && left == Open)
+}
+
 func splitRoom(t Former, bounds image.Rectangle) {
 	wall := makeSplitWall(bounds)
 	left, right := wall.Halves(bounds)
 	BspRooms(t, left)
 	BspRooms(t, right)
-	// TODO: Make a doorway along the wall.
+
+	doorSites := wall.DoorSites(t)
+	t.Set(doorSites[rand.Intn(len(doorSites))], Doorway)
 }
 
 type wall struct {
@@ -78,6 +115,33 @@ func (w wall) Length() int {
 		return w.End.Y - w.Begin.Y
 	}
 	return w.End.X - w.Begin.X
+}
+
+func (w wall) Dir() image.Point {
+	if w.IsVertical() {
+		return image.Pt(0, 1)
+	}
+	return image.Pt(1, 0)
+}
+
+func (w wall) Sides() (left, right image.Point) {
+	if w.IsVertical() {
+		return image.Pt(-1, 0), image.Pt(1, 0)
+	}
+	return image.Pt(0, -1), image.Pt(0, 1)
+}
+
+// DoorSites returns points along the wall which are suitable for placing a
+// doorway. Such points are ones which have open space on both of their sides.
+func (w wall) DoorSites(t Former) (result []image.Point) {
+	result = []image.Point{}
+	for pt := w.Begin; pt != w.End; pt = pt.Add(w.Dir()) {
+		left, right := w.Sides()
+		if t.At(pt.Add(left)) == Open && t.At(pt.Add(right)) == Open {
+			result = append(result, pt)
+		}
+	}
+	return
 }
 
 func (w wall) Halves(parent image.Rectangle) (left, right image.Rectangle) {
