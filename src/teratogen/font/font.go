859d397cb464ff2284b5d28c833be6b64abfde79
Risto Saarelma <risto.saarelma@iki.fi>
2012-08-04 13:51:50 +0300

    Change the license boilerplate to use line comments
    The "// +build" directives weren't being registered when they came after block
    comments.

--- src/teratogen/font/font.go
+++ src/teratogen/font/font.go
@@ -1,20 +1,19 @@
-/* font.go
-
-   Copyright (C) 2012 Risto Saarelma
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// font.go
+//
+// Copyright (C) 2012 Risto Saarelma
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Package font reads TrueType font files and renders them into bitmaps that
 // can be used in games. It uses the TrueType rasterizer v0.6c from Sean
